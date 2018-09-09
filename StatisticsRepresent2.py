22c29b944363fa48203bb496acfc5cded97f28f2
H.F <moyuejian@outlook.com>
2018-09-09 22:45:46 +0800

    use Absolute-value norm to choose worm

--- StatisticsRepresent2.py
+++ StatisticsRepresent2.py
@@ -2,7 +2,8 @@
 '''Script to represent worm track and statistics
 |Version | Author | Commit
 |0.1     | ZhouXY | first runable version
-|0.2.0     | H.F.   | don't show the image but only save image
+|0.2.0   | H.F.   | don't show the image but only save image
+|0.2.1   | H.F.   | use Absolute-value norm to choose continued worm
 ToDo: add quanlity report
 '''
 
@@ -36,32 +37,22 @@ def ExtractValidPoint(pos_data):
     valid_points = pos_data[:,(0,1)] # init point matrix with temp data
     for i in np.arange(pos_data.shape[1]//2): # read each one worm localtion
         [x,y] = (pos_data[:, 2*i], pos_data[:, 2*i+1])
-        '''dis = 0
-        for j in range(len(x)-1):
-            distance = ((x[j+1]-x[j])**2+(y[j+1]-y[j])**2)**(1/2)
-            if distance <40:
-                dis+=distance
-        if dis > 200:   # add the worm
-        '''
         if (max(x)-min(x)+max(y)-min(y) > 15):
             valid_points = np.column_stack((valid_points, x, y))
     return valid_points[:, 2::] # delete the first temp point
 
 # Remove trait that are long discontinued segment
-def ExtractContinuesPoint(pos_data,gap = 5):
+def ExtractContinuesPoint(pos_data,gap_num = 1):
+    gap_len = 30
     dis = True
     count = 0
-    continued_points = pos_data[:,(0,1)] # init point matrix with temp data
-    for i in np.arange(pos_data.shape[1]//2): # read each single worm
-        [x,y] = [pos_data[:, 2*1], pos_data[:,2*i+1]]
-        for j in range(len(x)-1): # judge between frame
-            distance = ((x[j+1]-x[j])**2+(y[j+1]-y[j])**2)**(1/2)
-            if distance >140:
-                count+=1
-                if count == gap:
-                    dis = False
-                    break
-        if dis:
+    continued_points = pos_data[:,(0,1)] # init point matrix with first worm
+    pos_diff = pos_data[:-1:,:] - pos_data[1::,:] # postion diff between frame
+    gap = (abs(pos_diff[:,0::2]) + abs(pos_diff[:,1::2]) > gap_len)
+    continued = (sum(gap) < gap_num)
+    for i in np.arange(continued.size): #  each single worm
+        [x,y] = [pos_data[:, 2*i], pos_data[:,2*i+1]]
+        if (continued[i]):
             continued_points = np.column_stack((continued_points,x,y))
     return continued_points[:, 2::] # delete 1st temp worm
 
@@ -69,10 +60,8 @@ if __name__ == "__main__":
     result_path = "/home/hf/iGEM/Results/20180904"
     pos_dat = genfromtxt('/home/hf/iGEM/Results/20180904/DiffussionTest7min.local',delimiter='\t')
     #PointPlot(pos_dat[:,(0,1,2,3,4,5)], "%s/PointImage"%result_path, 2)
-
-    #print("points",len(Pointdynamics))
     ValidPoints = ExtractValidPoint(pos_dat)
     print("validpoints: %i"%(ValidPoints.shape[1]//2)) 
-    PointPlot(ValidPoints, "%s/ValidPoint"%result_path, 2)
-    #ContinuedPoints = ExtractContinuesPoint(ValidPoints)
+    #PointPlot(ValidPoints, "%s/ValidPoint"%result_path, 2)
+    ContinuedPoints = ExtractContinuesPoint(ValidPoints)
     #PointPlot(ContinuedPoints, "%s/ContinuedPoints"%result_path, 2)

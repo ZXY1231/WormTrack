1d533869076902619716f9cf039ecb72dad77241
H.F <moyuejian@outlook.com>
2018-09-09 23:56:41 +0800

    draw the distrubtion in two direction

--- StatisticsRepresent2.py
+++ StatisticsRepresent2.py
@@ -4,31 +4,38 @@
 |0.1     | ZhouXY | first runable version
 |0.2.0   | H.F.   | don't show the image but only save image
 |0.2.1   | H.F.   | use Absolute-value norm to choose continued worm
+|0.2.2   | H.F.   | Draw the distrbution in two direction
 ToDo: add quanlity report
 '''
 
-import os,sys
-import re
 import numpy as np
-from numpy import genfromtxt
-import linecache
 import matplotlib.pyplot as plt
 
 # Plot the track image from([x], [y], frame rate)
 # To Do: add distribution aside image
 def PointPlot(pos_data, path, fram_rate):
     plt.clf()
-    fig,ax = plt.subplots()
-    ax.set_xlim([0,2000])
-    ax.set_ylim([0,2000])
-    print("Frame",len(pos_data))
-    for i in range(len(pos_data)):
+    for i in range(len(pos_data)): 
+        plt.subplot(2,2,3)
+        plt.xlim([0,2000])
+        plt.ylim([0,2000])
         x = pos_data[i,0::2]
         y = pos_data[i,1::2]
-        ax.plot(x,y,'ro',MarkerSize = 0.5)
-        ax.set_title("Frame: %i  %is/%is"%(i, i/fram_rate, len(pos_data)/fram_rate))
+        plt.plot(x,y,'ro',MarkerSize = 0.5)
+        
+        plt.subplot(2,2,1) # Histrogram in x-axis
+        plt.hist(x, bins=20, color='r')
+        plt.xlim([0,2000])
+        plt.ylim([0,20])
+        plt.title("Frame: %i  %is/%is"%(i, i/fram_rate, len(pos_data)/fram_rate))
+
+        plt.subplot(2,2,4) # Histrogram in y-axis
+        plt.hist(y, bins=20, color='r', orientation='horizontal')
+        plt.ylim([0,2000])
+        plt.xlim([0,20])
+
         print("drawing %i/%i frame"%(i, len(pos_data)))
-        plt.savefig("%s/%i.png"%(path,i),dpi=100) # save the image to folder
+        plt.savefig("%s/%i.png"%(path,i),dpi=200) # save the image to folder
         #plt.pause(0.002)
 
 # Remove the freeze points, such as dust
@@ -56,12 +63,15 @@ def ExtractContinuesPoint(pos_data,gap_num = 1):
             continued_points = np.column_stack((continued_points,x,y))
     return continued_points[:, 2::] # delete 1st temp worm
 
+
+
 if __name__ == "__main__":
     result_path = "/home/hf/iGEM/Results/20180904"
-    pos_dat = genfromtxt('/home/hf/iGEM/Results/20180904/DiffussionTest7min.local',delimiter='\t')
+    pos_dat = np.loadtxt('%s/DiffussionTest7min.local'%result_path,delimiter='\t')
     #PointPlot(pos_dat[:,(0,1,2,3,4,5)], "%s/PointImage"%result_path, 2)
     ValidPoints = ExtractValidPoint(pos_dat)
     print("validpoints: %i"%(ValidPoints.shape[1]//2)) 
     #PointPlot(ValidPoints, "%s/ValidPoint"%result_path, 2)
     ContinuedPoints = ExtractContinuesPoint(ValidPoints)
+    np.savetxt("%s/continued.local"%result_path,ContinuedPoints,delimiter='\t')
     #PointPlot(ContinuedPoints, "%s/ContinuedPoints"%result_path, 2)
